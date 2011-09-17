44a20b04c261beb4eedf0548f26af02186d3b540
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-09-17 11:20:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheHoneyPieStarChart::Application.config.secret_token = 'cff13b1c97658c17c3c18150da36c1e6ed2893cbdefc7757f62e68a6ba3ca7fbdba5b7ec1345d7821a1baff4db322e90856c795a97b559ac9f10599e1602da1d'
