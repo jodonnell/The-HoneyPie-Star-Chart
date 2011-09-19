fedd9646cda28e738c93b26941ba787b0852d00a
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-09-19 01:09:52 -0400

    adding extra stars features

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,28 @@ class User < ActiveRecord::Base
 
   def convert_activities_to_stars
     if completed_minimum?
-      self.stars += 4
-      save
+      minimum_reward
+      extra_rewards
       reset_activities
+      save
     end
   end
 
   private
+  def minimum_reward
+    self.stars += 4
+    self.yoga -= 1
+    self.gym -= 1
+    self.meditation -= 1
+  end
+
+  def extra_rewards
+    self.stars += yoga * 2
+    self.stars += gym * 3
+    self.stars += meditation
+  end
+
+
   def completed_minimum?
     yoga >= 1 and gym >= 1 and meditation >= 1
   end
@@ -24,6 +39,5 @@ class User < ActiveRecord::Base
     self.yoga = 0
     self.gym = 0
     self.meditation = 0
-    save
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User do
   before(:each) do
     @user = User.new
     @user.stars = 0
+    set_minimum
   end
 
   it 'should have an amount of stars' do
@@ -11,16 +12,38 @@ describe User do
   end
   
   it 'should let you convert activities to stars' do
-    @user.yoga = 1
-    @user.meditation = 1
-    @user.gym = 1
-
     @user.convert_activities_to_stars
 
-    @user.stars.should == 1
+    @user.stars.should == 4
     @user.yoga = 0
     @user.meditation = 0
     @user.gym = 0
   end
+
+  it 'rewards extra stars for doing extra yoga' do
+    @user.yoga = 2
+    @user.convert_activities_to_stars
+    @user.stars.should == 6
+  end
+
+  it 'rewards extra stars for doing extra meditation' do
+    @user.meditation= 2
+    @user.convert_activities_to_stars
+    @user.stars.should == 5
+  end
+
+  it 'rewards extra stars for doing extra gym' do
+    @user.gym = 2
+    @user.convert_activities_to_stars
+    @user.stars.should == 7
+  end
+
+  def set_minimum
+    @user.yoga = 1
+    @user.meditation = 1
+    @user.gym = 1
+  end
+
+
 
 end
